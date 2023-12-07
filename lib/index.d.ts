4291139476f8cfb81559636ae6bd6940e7fa4121
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-12-07 10:08:56 +0900

    Configure TypeScript and add blank implementations
    For `@tsconfig/*` packages, see <https://github.com/tsconfig/bases>.

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * A test schema.
+ */
+export type TestSchema = {
+	// TODO
+};
+
+/**
+ * Test a rule with the specified schema.
+ */
+export function testRule(schema: TestSchema): void;
+
+/**
+ * Test configurations for a rule.
+ */
+export function testRuleConfigs(schema: TestSchema): void;
